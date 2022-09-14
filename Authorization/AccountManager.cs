2cd1606803950d4bff43f3b60a4201f2336609f0
Vladislav <Vladislav@MR-CREAKER>
2022-09-14 14:55:00 +0300

    Add project files.

---
+++ Authorization/AccountManager.cs
@@ -0,0 +1,96 @@
+﻿namespace Authorization
+{
+    public static class AccountManager
+    {
+        public static List<AccountInfo> Acounts { get; set; } = new List<AccountInfo>();
+        private static bool IsAuthorizated(AccountInfo user)
+        {
+            foreach (var item in Acounts)
+            {
+                if (user == item && item.IsAdmin && !item.IsBlock)
+                {
+                    user.IsAdmin = item.IsAdmin;
+                    user.IsBlock = item.IsBlock;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void AddAcount(AccountInfo user, AccountInfo addingAcount)
+        {
+            if (IsAuthorizated(user))
+            {
+                Add(addingAcount);
+            }
+        }
+        private static void Add(AccountInfo acount)
+        {
+            Acounts.Add(acount);
+        }
+        public static void RemoveByUserName(AccountInfo user, string UserName)
+        {
+            if (IsAuthorizated(user))
+            {
+                Remove(Acounts.First(x => x.UserName == UserName));
+            }
+        }
+        private static void Remove(AccountInfo acount)
+        {
+            Acounts.Remove(acount);
+        }
+        public static void UpdateByUserName(AccountInfo user, string UserName, AccountInfo updatingAcount)
+        {
+            if (IsAuthorizated(user))
+            {
+                Update(UserName, updatingAcount);
+            }
+        }
+        private static void Update(string UserName, AccountInfo acount)
+        {
+            Remove(Acounts.First(x => x.UserName == UserName));
+            Add(acount);
+        }
+        public class AccountInfo
+        {
+            public string UserName { get; init; }
+            public string Password { get; init; }
+            public bool IsAdmin { get; set; }
+            public bool IsBlock { get; set; }
+            public AccountInfo(string userName, string password, bool isAdmin = false, bool isBlock = false)
+            {
+                UserName = userName;
+                Password = password;
+                IsAdmin = isAdmin;
+                IsBlock = isBlock;
+            }
+
+            public static bool operator ==(AccountInfo first, AccountInfo second)
+            {
+                return first.UserName == second.UserName && first.Password == second.Password;
+            }
+            public static bool operator !=(AccountInfo first, AccountInfo second)
+            {
+                return !(first == second);
+            }
+            public override bool Equals(object? obj)
+            {
+                if (obj == null)
+                    return false;
+
+                if (GetType() != obj.GetType())
+                    return false;
+                AccountInfo other = (AccountInfo)obj;
+                return this == other;
+            }
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+            public override string ToString()
+            {
+                return $"UserName = {UserName},Password = {Password},IsAdmin = {IsAdmin}, IsBlock = {IsBlock}";
+            }
+        }
+    }
+   
+}
---
+++ Authorization/Program.cs
@@ -0,0 +1,72 @@
+﻿using Authorization;
+
+AccountManager.Acounts = new List<AccountManager.AccountInfo>() {
+        new AccountManager.AccountInfo("1", "1", true),
+        new AccountManager.AccountInfo("2", "2"),
+        new AccountManager.AccountInfo("3", "3", false, true)
+    };
+Console.WriteLine("User:");
+Console.Write("Input User Name : ");
+string UserName = Console.ReadLine();
+Console.Write("Input Password : ");
+string Password = Console.ReadLine();
+AccountManager.AccountInfo user = new AccountManager.AccountInfo(UserName, Password);
+Console.WriteLine("All Accounts");
+Console.WriteLine("__________________________");
+foreach (var item in AccountManager.Acounts)
+{
+    Console.WriteLine(item.ToString());
+}
+Console.WriteLine("--------------------------");
+Console.WriteLine("Update Account");
+Console.WriteLine("__________________________");
+Console.Write("Input updating User Name ");
+UserName = Console.ReadLine();
+Console.Write("Input  new User Name : ");
+string NewUserName = Console.ReadLine();
+Console.Write("Input new Password : ");
+string NewPassword = Console.ReadLine();
+Console.Write("Input  new admin property : ");
+bool IsAdmin = Convert.ToBoolean(Console.ReadLine());
+Console.Write("Input  new block property : ");
+bool IsBlock = Convert.ToBoolean(Console.ReadLine());
+AccountManager.UpdateByUserName(user, UserName, new AccountManager.AccountInfo(NewUserName, NewPassword,IsAdmin,IsBlock));
+Console.WriteLine("--------------------------");
+Console.WriteLine("All Accounts");
+Console.WriteLine("__________________________");
+foreach (var item in AccountManager.Acounts)
+{
+    Console.WriteLine(item.ToString());
+}
+Console.WriteLine("--------------------------");
+Console.WriteLine("Add new Account");
+Console.WriteLine("__________________________");
+Console.Write("Input  new User Name : ");
+NewUserName = Console.ReadLine();
+Console.Write("Input new Password : ");
+NewPassword = Console.ReadLine();
+Console.Write("Input  new admin property : ");
+IsAdmin = Convert.ToBoolean(Console.ReadLine());
+Console.Write("Input  new block property : ");
+IsBlock = Convert.ToBoolean(Console.ReadLine());
+AccountManager.AddAcount(user, new AccountManager.AccountInfo(NewUserName, NewPassword, IsAdmin, IsBlock));
+Console.WriteLine("--------------------------");
+Console.WriteLine("All Accounts");
+Console.WriteLine("__________________________");
+foreach (var item in AccountManager.Acounts)
+{
+    Console.WriteLine(item.ToString());
+}
+Console.WriteLine("--------------------------");
+Console.WriteLine("Remove Account");
+Console.WriteLine("__________________________");
+Console.Write("Input removing account User Name ");
+UserName = Console.ReadLine();
+AccountManager.RemoveByUserName(user, UserName);
+Console.WriteLine("--------------------------");
+Console.WriteLine("All Accounts");
+Console.WriteLine("__________________________");
+foreach (var item in AccountManager.Acounts)
+{
+    Console.WriteLine(item.ToString());
+}
